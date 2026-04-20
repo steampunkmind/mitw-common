dc779cc122ae1f8a286cbc00e3039a553f09902b
Steampunk Mind <asteampunkmind@gmail.com>
2026-04-20 19:53:17 -0400

    Allows empty GAM model in init.

--- objects/mitw.gd
+++ objects/mitw.gd
@@ -25,7 +25,8 @@ static func init(aim_model_dict: Dictionary, gam_model_dict: Dictionary) -> void
 	_aim_model.clear_model()
 	_gam_model.clear_model()
 	_aim_model.set_model(aim_model_dict)
-	_gam_model.set_model(gam_model_dict, _aim_model)
+	if gam_model_dict.size() > 0:
+		_gam_model.set_model(gam_model_dict, _aim_model)
 
 
 static func get_frame_count() -> int:
